14e8d47ef25fde21f90b492f779c3eca91e861e6
ElOuastaniAziz <designsyaz@gmail.com>
2021-02-04 01:22:52 +0100

    mejoras con js y jquery

--- js/app.js
+++ js/app.js
@@ -1,11 +1,24 @@
 
+const nav = document.querySelector('#container nav');
 const barNav = document.querySelector('#container nav .hamburger');
 const navLinks = document.querySelector('#container nav .nav-links');
 const links = document.querySelectorAll('#container nav .nav-links li');
 
 barNav.addEventListener('click',()=>{
     navLinks.classList.toggle('open');
+    nav.classList.toggle('fix');
     links.forEach((link)=>{
         link.classList.toggle("fade");
     });
 });
+
+const parrafo = document.querySelectorAll('section .post p');
+
+console.log(parrafo[1].textContent)//Así sacamos el texto de un parafo javascript poro
+
+console.log($( "section .post p:eq(0)" ).text()); //Así sacamos el texto de un p con Jquery
+console.log($( "section .post p:first" ).text())
+
+
+
+
